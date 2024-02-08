5c966189378ad2d39d54c17a35172f7b703764f4
Kyriakos Antoniadis <contact@ai-systems.today>
2024-02-08 19:00:21 +0000

    authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphql7bd4c31c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AmplifyUsersGroupRole": "string"
+    }
+  }
+}
